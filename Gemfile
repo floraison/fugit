a1d44c284195850087e4cbc306aa5aaaea89e23b
John Mettraux <jmettraux@gmail.com>
2019-01-13 06:22:57 +0900

    Align spec infra on et-orbi spec infra, gh-11

--- Gemfile
+++ Gemfile
@@ -8,5 +8,7 @@ source 'https://rubygems.org'
 #gem 'et-orbi', git: 'https://github.com/floraison/et-orbi'  # temporarily
 #gem 'et-orbi', path: '../et-orbi/'                          # temporarily
 
+#gem 'activesupport' # temporarily for gh-11
+
 gemspec
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,24 +8,46 @@
 require 'pp'
 #require 'ostruct'
 
+require 'chronic'
+::Khronic = ::Chronic
+Object.send(:remove_const, :Chronic)
+
 require 'fugit'
 
 
-#def jruby?
-#
-#  !! RUBY_PLATFORM.match(/java/)
-#end
+module Helpers
+
+  def jruby?; !! RUBY_PLATFORM.match(/java/); end
+  def windows?; Gem.win_platform?; end
 
 
-def in_zone(zone_name, &block)
+  def in_zone(zone_name, &block)
 
-  prev_tz = ENV['TZ']
-  ENV['TZ'] = zone_name if zone_name
+    prev_tz = ENV['TZ']
+    ENV['TZ'] = zone_name if zone_name
 
-  block.call
+    block.call
 
-ensure
+  ensure
 
-  ENV['TZ'] = prev_tz
+    ENV['TZ'] = prev_tz
+  end
+
+  def require_chronic
+
+    Object.const_set(:Chronic, Khronic)
+  end
+
+  def unrequire_chronic
+
+    Object.send(:remove_const, :Chronic)
+  end
+end # Helpers
+
+RSpec.configure do |c|
+
+  c.alias_example_to(:they)
+  c.alias_example_to(:so)
+  c.include(Helpers)
 end
 
